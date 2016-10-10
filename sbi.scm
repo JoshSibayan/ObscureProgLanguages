3e03b959ca1d52562851896cf0be19135ef54026
JoshSibayan <jbsibaya@ucsc.edu>
2016-10-10 15:36:52 -0700

    Cleanup

--- sbi.scm
+++ sbi.scm
@@ -61,14 +61,19 @@
 ;; program begins.
 (define *label-table* (make-hash))
 
+;; *variable-table* which holds the value of all variables. This is initialized
+;; with pi and e and is updated as needed during interpretation of the program.
+;; Arrays are created with make-vector and updated with vector-set!.
+(define *variable-table* (make-hash))
+
 ;; Referred to symbols.scm example
-(define *symbol-table* (make-hash))
-(define (symbol-put! key value)
-	(hash-set! *symbol-table* key value))
+;; Create and initialize variable-table
+(define (var-put! key value)
+	(hash-set! *variable-table* key value))
 
 (for-each
 	(lambda (pair)
-		(symbol-put! (car pair) (cadr pair)))
+		(var-put! (car pair) (cadr pair)))
 	`(
 
 		(log10_2 ,0.301029995663981195213738894724493026768189881)
@@ -107,58 +112,40 @@
 
 	))
 
-;; Note: REMEMBER NOT TO USE IMPERATIVE FUNCTIONS!
+;; Create hash table to translate sbir into scheme functions
+(for-each
+	(lambda (pair)
+		(hash-set! *function-table* (car pair) (cadr pair)))
+	`(
 
-;; Determine category of object
-;; Evaluate list recursively
-;; Referred to hashexample.scm example
-(define (eval-expr expr)
-	(cond ((string? expr) expr)
-	      ((number? expr) expr)
-	      ((hash-has-key? *symbol-table* expr)
-	              (hash-ref *symbol-table* expr))
-	      ((list? expr)
-	              (if (hash-has-key? *symbol-table* (car expr)) 
-		              (let ((head (hash-ref *symbol-table* (car expr))))
-				      (cond ((number? head) head)
-				      	    ((vector? head) (vector-ref head (cadr expr)))
-			                    ((procedure? head)
-				                    (apply head (map (lambda (x) (eval-expr x)) (cdr expr))))
-				      (else (die "Unable to evaluate espression type"))))
-			       (die (list (car expr) " not found in table\n"))))))
+	  	;; The two Expressions are compared according to the given Relop, and if
+		;; the comparison is true, control transfers to the statement, as for the
+		;; goto statement. Note : <> is the symbol for not equal. The others
+		;; should be obvious.
+		(if       (void))
+	  	;; Control transfers to the statement referred to by the Label. An error
+		;; occurs if the Label is not defined.
+		(goto     (void))
+		(print    ,eval-print)
+		(let      ,eval-let)
+		(dim      ,eval-dim)
+		(input    ,eval-input)
+		
+	))
 
-;; Translate sbir dim statements
-;; Declare array
-(define (eval-dim expr)
-	(set! expr (car expr))
-	(let ((arr (make-vector (eval-expr (cadr expr)) (car expr))))
-		(symbol-put! (car expr) (+ (eval-expr (cadr expr)) 1))))
-
-;; Translate sbir let statements
-;; Create variable
-(define (eval-let expr)
-	(symbol-put! (car expr) (eval-expr (cadr expr))))
-
-;; Translate sbir print statements
-;; Evaluate when given print argument
-(define (eval-print expr)
-	(map (lambda (x) (display (eval-expr x))) expr)
-	(newline))
-
-;; Yet another recursive function to keep track of count variable
-(define (input-count expr count)
-	(if (null? expr) count (let ((input (read))) (if (eof-object? input) -1
-		(begin (symbol-put! (car expr) input) 
-		(set! count (+ 1 count)) (input-count (cdr expr) count))))))
-
+;; Length helper function
 (define length (lambda (x)
 	(if (null? x) 0 (+ (length (cdr x)) 1))))
 
-;; Take input for symbol-table
-(define (eval-input expr)
-	(symbol-put! 'eval-count 0)
-	(if (null? (car expr)) 
-		(symbol-put! 'eval-count -1) (begin (symbol-put! 'eval-count (input-count expr 0)))))
+;; Put labels into hash table
+;; No need to tokenize input, sbir programs in list form already
+(define (put-label program)
+	(map (lambda (line)
+		(when (not (null? line))
+			(when (or (= 3 (length line)) 
+			      (and (= 2 (length line)) 
+                              (not (list? (cadr line)))))
+			(hash-set! *label-table* (cadr line) (- (car line) 1))))) program))
 
 ;; No practical loops in scheme, run repeatedly through tail-recursion using accumulator
 (define (eval-line program line-num)
@@ -187,15 +174,89 @@
 		      (else ((hash-ref *function-table* (car command)) (cdr command))
 		              (eval-line program (+ line-num 1)))))
 
-;; Put labels into hash table
-;; No need to tokenize input, sbir programs in list form already
-(define (put-label program)
-	(map (lambda (line)
-		(when (not (null? line))
-			(when (or (= 3 (length line)) 
-			      (and (= 2 (length line)) 
-                              (not (list? (cadr line)))))
-			(hash-set! *label-table* (cadr line) (- (car line) 1))))) program))
+;; Each of the operands is printed in sequence, with a space before Expression
+;; values. A newline is output at the end of the print statement.
+;; print statements are the only place Strings may occur in SBIR.
+;;
+;; Translate sbir print statements
+;; Evaluate when given print argument
+(define (eval-print expr)
+	(map (lambda (x) (display (eval-expr x))) expr)
+	(newline))
+
+;; Constants are numbers. Note that names of Functions, Arrays, and
+;; Variables all look like identifiers and their meaning is given by context.
+;; In particular, the syntax of a function call and an array subscript is
+;; ambiguous. The code for both is just to send a message to the Symbol
+;; table and get backaresult.
+;;
+;; Determine category of object
+;; Evaluate list recursively
+;; Referred to hashexample.scm example
+(define (eval-expr expr)
+	(cond ((string? expr) expr)
+	      ((number? expr) expr)
+	      ((hash-has-key? *variable-table* expr)
+	              (hash-ref *variable-table* expr))
+	      ((list? expr)
+	              (if (hash-has-key? *variable-table* (car expr)) 
+		              (let ((head (hash-ref *variable-table* (car expr))))
+				      (cond ((number? head) head)
+				      	    ((vector? head) (vector-ref head (cadr expr)))
+			                    ((procedure? head)
+				                    (apply head (map (lambda (x) (eval-expr x)) (cdr expr))))
+				      (else (die "Unable to evaluate espression type"))))
+			       (die (list (car expr) " not found in table\n"))))))
+
+;; The dim statement creates an array given by the variable name and
+;; inserts it into the Symbol table, replacing any previous variable, array,
+;; or function already in the Symbol table. The dimension of the array is
+;; given by the expression.
+;; Unlike C, the lower bound of the array is 1 and the upper bound is the
+;; dimension, which may be an arbitrary expression. The expression is
+;; rounded to the nearest integer before being used as the bound, which
+;; must be positive.
+;;
+;; Translate sbir dim statements
+;; Declare array
+(define (eval-dim expr)
+	(set! expr (car expr))
+	(let ((arr (make-vector (eval-expr (cadr expr)) (car expr))))
+		(var-put! (car expr) (+ (eval-expr (cadr expr)) 1))))
+
+;; A let statement makes an assignment to a variable. The expression is
+;; first evaluated. For a Variable, its value is stored into the Symbol table,
+;; replacing whatever was there previously. For an Array, the store message
+;; is sent to the vector representing the array. If the Symbol table
+;; entry is not an array, an error occurs.
+;;
+;; Translate sbir let statements
+;; Create variable
+(define (eval-let expr)
+	(var-put! (car expr) (eval-expr (cadr expr))))
+
+;; Yet another recursive function to keep track of count variable
+(define (input-count expr count)
+	(if (null? expr) count (let ((input (read))) (if (eof-object? input) -1
+		(begin (var-put! (car expr) input) 
+		(set! count (+ 1 count)) (input-count (cdr expr) count))))))
+
+;; Numeric values are read in and assigned to the input variables in
+;; sequence. Arguments might be elements of an array. For each value
+;; read into a Variable, the value is inserted into the Symbol table under
+;; that variable’s key. For arrays, the array must already exist and the
+;; subscript not be out of bounds.
+;; The variable inputcount is inserted into the symbol table at end of execution
+;; of this statement and initialized to the number of values successfully
+;; read in. A value of −1 is returned to indicate end of file. If anything
+;; other than a number occurs, that token is discarded, an error message
+;; is printed, and scanning continues.
+;;
+;; Take input for variable-table
+(define (eval-input expr)
+	(var-put! 'eval-count 0)
+	(if (null? (car expr)) 
+		(var-put! 'eval-count -1) (begin (var-put! 'eval-count (input-count expr 0)))))
 
 ;; Check validity of arguments passed, else return usage-exit
 ;; When argument valid, set sbprogfile equal to it
@@ -207,19 +268,4 @@
 		       (program (readlist-from-inputfile sbprogfile)))
 		       (put-label program) (eval-line program 0))))
 			
-;; Create hash table to translate sbir into scheme functions
-(for-each
-	(lambda (pair)
-		(hash-set! *function-table* (car pair) (cadr pair)))
-	`(
-
-		(if       (void))
-		(goto     (void))
-		(print    ,eval-print)
-		(let      ,eval-let)
-		(dim      ,eval-dim)
-		(input    ,eval-input)
-		
-	))
-
 (main (vector->list (current-command-line-arguments)))
